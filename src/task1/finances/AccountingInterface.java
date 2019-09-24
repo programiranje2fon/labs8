3217c586905cafca987dd08bf914c9bb21c6a2f0
bojantomic <tomicb@fon.rs>
2019-09-24 14:11:38 +0200

    Deleted solutions

--- src/task1/finances/AccountingInterface.java
+++
@@ -1,8 +0,0 @@
-package task1.finances;
-
-import task1.Employee;
-
-public interface AccountingInterface {
-
-	public void paySalaries(Employee[] employees, int workingHours);
-}
--- src/task2/ATMInterface.java
+++
@@ -1,8 +0,0 @@
-package task2;
-
-public interface ATMInterface {
-
-	public void depositMoney(double amount);
-
-	public void withdrawMoney(double amount);
-}
